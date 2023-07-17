ac73657bf8ea395f300696f032ea9d87afd2600c
ASHISHSINGHNEGI <ashishsinghnegi381@gmail.com>
2023-07-17 12:17:02 +0530

    initial commint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-function App() {
+function App(props) {
+
+  const [tasks, setTasks] = useState("");
+
+  function addTask(name) {
+    console.log("handleSubmit call addTask")
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false }
+    console.log("new task is ", newTask)
+    console.log("new task list is ", typeof [...tasks, newTask])
+
+    setTasks([...tasks, newTask]);
+
+  }
+  function toggleTaskCompleted(id) {
+    console.log(tasks[0]);
+  }
+
+  console.log("task var type", typeof props.tasks)
+  const taskList = props.tasks.map(
+    (task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted}
+    />
+  );
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  console.log("task list at begining is ", taskList)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+
+
+        {taskList}
+      </ul>
     </div>
   );
 }
-
 export default App;
---
+++ src/component/list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function List() {    //always name function in sentence case
+    return (
+        <>
+            <div className="container">
+                <button className="btn btn"></button>
+            </div>
+
+        </>
+    )
+}
